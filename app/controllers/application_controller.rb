e0f39d63525ea9cfd945b0b23a31c47d76ca05f2
aiyuhang <aiyuhang@wityun.com>
2020-11-22 22:35:19 +0800

    finished test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :is_admin?
   def current_user
+    return User.first if Rails.env.test?
     User.find_by(id: session[:user_id])
   end
 end
--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,4 +1,5 @@
 class StatisticsController < ApplicationController
+  before_action :check_user_admin, only: [:index]
   def index
     steams = Steam.last(100)
     @name_arr = steams.pluck(:name)
@@ -7,4 +8,11 @@ class StatisticsController < ApplicationController
     @positive_ratings = steams.pluck(:positive_ratings)
     @negative_ratings = steams.pluck(:negative_ratings)
   end
+
+  private
+    def check_user_admin
+      unless current_user&.is_admin?
+        redirect_to root_path, notice: "You do not have permission to access this interface!"
+      end
+    end
 end
