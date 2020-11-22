8fc49ceb2f4483cc0a535c9db8644e32f83274d7
aiyuhang <aiyuhang@wityun.com>
2020-11-22 22:26:41 +0800

    add charts

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,4 +1,10 @@
 class StatisticsController < ApplicationController
   def index
+    steams = Steam.last(100)
+    @name_arr = steams.pluck(:name)
+    @achievement_arr = steams.pluck(:achievements)
+
+    @positive_ratings = steams.pluck(:positive_ratings)
+    @negative_ratings = steams.pluck(:negative_ratings)
   end
 end
